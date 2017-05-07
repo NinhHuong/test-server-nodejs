b819d98395ddb6a39a822847491799c646c57b1d
ninhhuong <ninhhuongdst@gmail.com>
2017-05-06 20:03:53 -0700

    demo add, edit, delete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -8,6 +8,114 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+var pg = require('pg');
+config = {
+  user: 'foxyqsiwlnqyjv',
+  database: 'd4ug5pkn3lbq6t',
+  password: '284d7bd04696101423ce32fbd8bd06c3a3e0f9c5baedc12a1128f278101a14d3',
+  host: 'ec2-23-23-228-115.compute-1.amazonaws.com',
+  port: 5432,
+  ssl: true,
+  max: 10,
+  idleTimeoutMillis: 30000,
+}
+//init a connection pool
+//keep connections open for 30 secondes
+//set limit of max 10 idle clients
+var pool = new pg.Pool(config);
+
+//show list
+app.get("/student/list", function(req, res) {
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM student', function(err, result) {
+      //call 'done()' to release the client back to the pool
+      done();
+
+      if(err) {
+        res.end();
+        return console.error('error running query', err);
+      }
+      console.log("total row: ", result.rows.length);
+
+      res.render("student_list.ejs", {list_result:result});
+    });
+  });
+});
+
+//insert
+app.get("/student/add", function(req, res) {
+  pool.connect(function(err, client, done) {
+    res.render("student_insert.ejs");
+  });
+});
+
+app.post("/student/add", urlencodedParser, function(req, res) {
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    var fullname = req.body.txt_full_name;
+    var email = req.body.txt_email;
+    client.query("INSERT INTO student (fullname,email) VALUES ('" + fullname + "','" + email + "')", function(err, result) {
+      //call 'done()' to release the client back to the pool
+      done();
+
+      if(err) {
+        res.end();
+        return console.error('error running query', err);
+      }
+      res.redirect("../student/list");
+    });
+  });
+});
+
+//edit
+app.get("/student/edit/:id", function(req, res) {
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    var id = req.params.id;
+    client.query("SELECT * FROM student WHERE id='" + id + "'", function(err, result) {
+      //call 'done()' to release the client back to the pool
+      done();
+
+      if(err) {
+        res.end();
+        return console.error('error running query', err);
+      }
+      console.log("total row: ", result.rows.length);
+
+      res.render("student_edit.ejs", {student:result.rows[0]});
+    });
+  });
+});
+app.post("/student/edit", urlencodedParser, function(req, res) {
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    var id = req.body.txt_id;
+    var fullname = req.body.txt_full_name;
+    var email = req.body.txt_email;
+    client.query("UPDATE student SET fullname='" + fullname + "', email='" + email + "' WHERE id='" + id + "'", function(err, result) {
+      //call 'done()' to release the client back to the pool
+      done();
+
+      if(err) {
+        res.end();
+        return console.error('error running query', err);
+      }
+      res.redirect("../student/list");
+    });
+  });
+});
+
 
 app.get('/', function(request, response) {
   response.render('main');
